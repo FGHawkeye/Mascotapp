8a2e612d670c3fec28b79707fb64b2ec462585c6
Gabriel Ajaya <gabriel.ajaya@sarsyl-srl.com>
2019-10-12 16:52:51 -0300

    Se agrega opcion de navegacion para "ALTA MARCADOR" ademas de controles para la pagina de Alta marcador.

--- Mascotapp/Mascotapp/MainPage.xaml.cs
+++ Mascotapp/Mascotapp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Mascotapp.NavigationMenu;
 using Mascotapp.Visualizar_mapa;
+using System;
 using Xamarin.Forms;
 
 namespace Mascotapp
@@ -15,8 +16,11 @@ namespace Mascotapp
             InitializeComponent();
             this.Master = new Master();
             this.Detail = new NavigationPage(new Mapa());
+
+            App.MasterD = this;
             //CargarUsuarios();
         }
+
 
         //void CargarUsuarios()
         //{
--- Mascotapp/Mascotapp/NavigationMenu/Master.xaml.cs
+++ Mascotapp/Mascotapp/NavigationMenu/Master.xaml.cs
@@ -13,55 +13,15 @@ namespace Mascotapp.NavigationMenu
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Master : ContentPage
     {
-        public ListView ListView { get { return listView; } }
-
-        ListView listView;
-
-
         public Master()
         {
             InitializeComponent();
-            CargarItems();
         }
 
-        void CargarItems()
+        private async void BtnAgregarMarcador_Clicked(object sender, EventArgs e)
         {
-            var masterPageItems = new List<MasterPageItem>();
-            masterPageItems.Add(new MasterPageItem
-            {
-                Title = "Agregar marcador",
-                IconSource = "contacts.png",
-                TargetType = typeof(AltaMarcador)
-            });
-
-
-            listView = new ListView
-            {
-                ItemsSource = masterPageItems,
-                ItemTemplate = new DataTemplate(() => {
-                    var grid = new Grid { Padding = new Thickness(5, 10) };
-                    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(30) });
-                    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
-
-                    var image = new Image();
-                    image.SetBinding(Image.SourceProperty, "IconSource");
-                    var label = new Label { VerticalOptions = LayoutOptions.FillAndExpand };
-                    label.SetBinding(Label.TextProperty, "Title");
-
-                    grid.Children.Add(image);
-                    grid.Children.Add(label, 1, 0);
-
-                    return new ViewCell { View = grid };
-                }),
-                SeparatorVisibility = SeparatorVisibility.None
-            };
-
-            IconImageSource = "hamburger.png";
-            Title = "Personal Organiser";
-            Content = new StackLayout
-            {
-                Children = { listView }
-            };
+            App.MasterD.IsPresented = false; //isVisible = false
+            await App.MasterD.Detail.Navigation.PushAsync(new AltaMarcador());
         }
     }
 }
