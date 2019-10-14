0fdc90aadd7daa2990a84be32f3870c525aa1545
fjv2019 <fversari@gmail.com>
2019-10-13 23:39:38 -0300

    Se creo_Xaml Notificaciones & Servicio Notificaciones.

---
+++ Domain/Servicios/ServicioNotificaciones.cs
@@ -0,0 +1,29 @@
+﻿using Domain.DB;
+using Domain.Entidades;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Domain.Servicios
+{
+    public class ServicioUsuarios
+    {
+        SQLiteConnection dbConnection;
+        public ServicioUsuarios()
+        {
+            dbConnection = DependencyService.Get<IDBInterface>().CreateConnection();
+        }
+
+        public List<Usuario> ObtenerUsuarios()
+        {
+            return dbConnection.Query<Usuario>("Select * From [Usuarios]");
+        }
+
+        public int GuardarUsuario(Usuario usuario)
+        {
+            return dbConnection.Insert(usuario);
+        }
+    }
+}
