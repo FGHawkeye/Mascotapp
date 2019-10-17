73a281430c10a9670127bd4ace1ec9ce83f71354
Gabriel Ajaya <gabriel.ajaya@sarsyl-srl.com>
2019-10-17 00:08:24 -0300

    Se agrega funcionalidad para poder sacar fotos con la camara (se agrega nueva dependencia, nuevos permisos requeridos y se indica los path donde iran las imagenes) Se agrega prototipo de guardado de entidad en base de datos.

--- Mascotapp/Mascotapp/Marcador animales/AltaMarcador.xaml.cs
+++ Mascotapp/Mascotapp/Marcador animales/AltaMarcador.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Domain.Entidades;
+using Domain.Servicios;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +17,58 @@ namespace Mascotapp.Marcador_animales
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AltaMarcador : ContentPage
     {
+        #region BindableObjects
+        List<TipoAnimal> _lstTipoAnimal = new List<TipoAnimal>();
+        #endregion
+
+        private ServicioTipoAnimal serviceTipoAnimal = new ServicioTipoAnimal();
+        private ServicioMarcadores serviceMarcadores = new ServicioMarcadores();
+
         public AltaMarcador()
         {
             InitializeComponent();
+            CargarControles();
+            CargarEventos();
+
         }
+
+        void CargarEventos()
+        {
+            btnCamera.Clicked += btnCamera_Clicked;
+            btnAgregar.Clicked += btnAgregar_Clicked;
+        }
+
+        void CargarControles()
+        {
+            CargarTipoAnimales();
+        }
+
+        void CargarTipoAnimales()
+        {
+            _lstTipoAnimal = serviceTipoAnimal.ObtenerTipoAnimales();
+            pckTipoAnimal.ItemsSource = _lstTipoAnimal;
+        }
+
+        private async void btnCamera_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                imgCamara.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
+        private void btnAgregar_Clicked(object sender, EventArgs e)
+        {
+            var marcador = new Marcadores();
+            marcador.IdTipoAnimal = 1;//pckTipoAnimal.SelectedItem.
+            marcador.Descripcion = txtDescripcion.Text;
+            marcador.IdImagen = 1;
+            marcador.IdUsuario = 1;
+            marcador.IdMarcador = 1;
+            marcador.Estado = "wea";
+            marcador.Ubicacion = "wea";
+            serviceMarcadores.GuardarMarcador(marcador);
+        }
+
     }
 }
