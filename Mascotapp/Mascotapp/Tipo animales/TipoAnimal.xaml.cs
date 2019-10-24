c7541f657c05297ac0f35ee32f529502caf1e6e7
tomy_herman <tomy_herman@hotmail.com>
2019-10-24 11:34:59 -0300

    Se agrego: Entidades, Servicios

--- Mascotapp/Mascotapp/Tipo animales/TipoAnimal.xaml.cs
+++ Mascotapp/Mascotapp/Tipo animales/TipoAnimal.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,28 @@ namespace Mascotapp.Tipo_animales
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TipoAnimales : ContentPage
     {
+        #region BindableObjects
+        List<TipoAnimal> _lstTipoAnimal = new List<TipoAnimal>();
+        #endregion
+
+        private ServicioTipoAnimales serviceTipoAnimal = new ServicioTipoAnimales();
+
         public TipoAnimales()
         {
             InitializeComponent();
+            CargarControles();
+            CargarEventos();
+        }
+
+        void CargarControles()
+        {
+            CargarTipoAnimales();
+        }
+
+        void CargarEventos()
+        {
+            //btnCamera.Clicked += btnCamera_Clicked;
+            btnAgregar.Clicked += btnAgregar_Clicked;
         }
     }
 }
