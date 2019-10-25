e12ce65762aa1451668a42d8935c9f817313f217
Mati <matias.casas@hotmail.com.ar>
2019-10-24 22:32:27 -0300

    Login Actualizado v1

--- Mascotapp/Mascotapp/Login/Login.xaml.cs
+++ Mascotapp/Mascotapp/Login/Login.xaml.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entidades;
+using Domain.Servicios;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace Mascotapp.Login
 {
