432e1aec174e1c7b82a556e8f1a1c8064671ef3d
Gabriel Ajaya <gabriel.ajaya@sarsyl-srl.com>
2019-11-09 00:51:37 -0300

    Se agrega validacion de refugios en estado rechazado y pendiente

--- Mascotapp/Mascotapp/Login/Login.xaml.cs
+++ Mascotapp/Mascotapp/Login/Login.xaml.cs
@@ -14,17 +14,18 @@ using Mascotapp.NavigationMenu;
 
 namespace Mascotapp.Login
 {
-    
-    
+
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
 
         private ServicioUsuarios servicioUsuarios = new ServicioUsuarios();
+        private ServicioRefugio servicioRefugio = new ServicioRefugio();
 
         public Login(string nombreusuario = "", string contra = "")
         {
-            
+
             InitializeComponent();
             CargarEventos();
 
@@ -53,36 +54,53 @@ namespace Mascotapp.Login
             {
                 Usuario Usuario = new Usuario();
                 Usuario UsuarioValidado = new Usuario();
-                
+
                 Usuario.NombreUsuario = txtNombre.Text.Trim();
-                Usuario.Contraseña =  txtContra.Text.Trim();
+                Usuario.Contraseña = txtContra.Text.Trim();
 
                 UsuarioValidado = servicioUsuarios.ValidarUsuario(Usuario);
 
-
-                MainPage.UsuarioRegristrado = UsuarioValidado;
-
-
-                if (UsuarioValidado.IdTipoUsuario == 1)
+                if (UsuarioValidado.IdTipoUsuario == 3)
                 {
-                    App.MasterD.Master = new MenuAdmin();
+                    var refugio = servicioRefugio.ObtenerRefugioUsuario(UsuarioValidado.IdUsuario.Value);
+
+                    if (refugio.Estado == "Pendiente")
+                    {
+                        await DisplayAlert("Error de autenticación", "Su cuenta de refugio esta en estado pendiente a revisar por un administrador, no puede utilizar momentaneamente esta cuenta", "Entendido");
+                        await Navigation.PopAsync(false);
+                    }
+                    else if (refugio.Estado == "Rechazado")
+                    {
+                        await DisplayAlert("Error de autenticación", "Su cuenta de refugio fue rechazado por un administrador, su cuenta esta deshabilitada", "Entendido");
+                        await Navigation.PopAsync(false);
+                    }
                 }
                 else
                 {
-                    App.MasterD.Master = new MenuUsuario();
+                    MainPage.UsuarioRegristrado = UsuarioValidado;
+
+                    if (UsuarioValidado.IdTipoUsuario == 1)
+                    {
+                        App.MasterD.Master = new MenuAdmin();
+                    }
+                    else
+                    {
+                        App.MasterD.Master = new MenuUsuario();
+                    }
+
+                    await Navigation.PopAsync(false);
+
+                    await DisplayAlert("Bienvenido " + MainPage.UsuarioRegristrado.Nombre, "Nos alegra que nos visites nuevamente.", "Continuar");
+
+                    await Navigation.PopAsync(false);
                 }
 
-                await Navigation.PopAsync(false);
-
-                await DisplayAlert("Bienvenido " + MainPage.UsuarioRegristrado.Nombre, "Nos alegra que nos visites nuevamente.", "Continuar");
-
-                await Navigation.PopAsync(false);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-  
-                    await DisplayAlert("Error de autenticación", "Su cuenta o contraseña no se encuentran registradas", "Entendido");
-                    txtContra.Text = "";
+
+                await DisplayAlert("Error de autenticación", "Su cuenta o contraseña no se encuentran registradas", "Entendido");
+                txtContra.Text = "";
             }
 
 
@@ -92,7 +110,7 @@ namespace Mascotapp.Login
 
         private void Cancelar_Clicked(object sender, EventArgs e)
         {
-           Navigation.PopAsync(false);
+            Navigation.PopAsync(false);
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
